b04e4030db82ee5ae2506f40e460f5a1c1b05fb1
Abbos <abbosnorqulov2002@gmail.com>
2024-09-25 21:59:21 +0500

    red

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+
+
+
+
+
+import React, { useRef, useState } from 'react';
 import './App.css';
+
+const VideoPlayer = () => {
+  const videoRef = useRef(null);
+  const [playbackRate, setPlaybackRate] = useState(1);
+  const [videoSrc, setVideoSrc] = useState(null);
+  const [fileName, setFileName] = useState('No file chosen'); // Fayl nomi
+
+  const handleSpeedChange = (event) => {
+    const newSpeed = parseFloat(event.target.value);
+    setPlaybackRate(newSpeed);
+    videoRef.current.playbackRate = newSpeed;
+  };
+
+  const handleVideoUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setVideoSrc(url);
+      setFileName(file.name); // Fayl nomini yangilash
+    }
+  };
+
+  return (
+    <div className="video-player-container">
+      <div className="custom-file-upload">
+        <label htmlFor="file-upload" className="custom-upload-button">
+          Upload video
+        </label>
+        <input
+          id="file-upload"
+          type="file"
+          accept="video/*"
+          onChange={handleVideoUpload}
+        />
+        <span>{fileName}</span>
+      </div>
+      {videoSrc ? (
+        <div>
+          <video ref={videoRef} width="600" controls src={videoSrc}>
+            Your browser does not support the video.
+          </video>
+          <div className="speed-control">
+            <label>Change speed: </label>
+            <input
+              type="range"
+              min="0.07"
+              max="2"
+              step="0.01"
+              value={playbackRate}
+              onChange={handleSpeedChange}
+            />
+            <span>{playbackRate}x</span>
+          </div>
+        </div>
+      ) : (
+        <p>Please upload a video.</p>
+      )}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Video Player with Speed Control</h1>
+      <VideoPlayer />
     </div>
   );
 }
